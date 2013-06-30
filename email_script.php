4b66a1e23b7302b8340e4c83f798fa40fec2f6b2
root <root@ip-10-251-63-36.us-west-2.compute.internal>
2013-06-30 02:42:00 +0000

    Switched to phpmailer

--- email_script.php
+++ email_script.php
@@ -1,33 +1,24 @@
-<?php 
- $to = $_REQUEST['sendto'] ; 
- $from = $_REQUEST['Email'] ; 
- $name = $_REQUEST['Name'] ; 
- $headers = "From: $from"; 
- $subject = "Web Contact Data"; 
+<?php
+  require_once 'lib/swift_required.php';
  
- $fields = array(); 
- $fields{"Name"} = "Name"; 
- $fields{"Company"} = "Company"; 
- $fields{"Email"} = "Email"; 
- $fields{"Phone"} = "Phone"; 
- $fields{"list"} = "Mailing List"; 
- $fields{"Message"} = "Message"; 
+  //Create the Transport
+  $transport = new Swift_AWSTransport(
+    'AKIAI266ZFDPXSSDRGAA',
+    'xY1D4BkQ4QIQIisL0uTu4Bdw6LJJMY0nLUHglbh8'
+  );
  
- $body = "Hi diego and marco this is tiffany from inside your computer:\n\n"; foreach($fields as $a => $b){ 	$body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+  //Create the Mailer using your created Transport
+  $mailer = Swift_Mailer::newInstance($transport);
  
- $headers2 = "From: congratulatinos@gmail.com";
- $subject2 = "Thank you for contacting us"; 
- $autoreply = "Hey Diego it's Ryan I am stuck inside a computer! I figured out how to use this webserver!"
- if($from == '') {print "You have not entered an email, please go back and try again";} 
- else { 
- if($name == '') {print "You have not entered a name, please go back and try again";} 
- else { 
- $send = mail($to, $subject, $body, $headers); 
- $send2 = mail($from, $subject2, $autoreply, $headers2); 
- if($send) 
- {header( "Location: http://www.google.com/" );}
- else 
- {print "We encountered an error sending your mail, please notify webmaster@YourCompany.com"; } 
- }
-}
- ?> 
+  //Create the message
+  $message = Swift_Message::newInstance()
+  ->setSubject("What up?")
+  ->setFrom(array('congratulatinos@gmail.com'))
+  ->setTo(array('congratulatinos@gmail.com'))
+  ->setBody("
+<p>
+  Dude, I'm <b>totally</b> sending you email via AWS.
+</p>
+  ", 'text/html');
+ 
+  $mailer->send( $message );
