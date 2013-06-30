86ab4a87ea3c0ee7291cfd1fa979ac68ee64f008
root <root@ip-10-251-63-36.us-west-2.compute.internal>
2013-06-30 03:45:07 +0000

    undoing accindetal email script commit

--- email_script.php
+++ email_script.php
@@ -1,24 +1,32 @@
 <?php
-  require_once 'lib/swift_required.php';
- 
-  //Create the Transport
-  $transport = new Swift_AWSTransport(
-    'AKIAI266ZFDPXSSDRGAA',
-    'xY1D4BkQ4QIQIisL0uTu4Bdw6LJJMY0nLUHglbh8'
-  );
- 
-  //Create the Mailer using your created Transport
-  $mailer = Swift_Mailer::newInstance($transport);
- 
-  //Create the message
-  $message = Swift_Message::newInstance()
-  ->setSubject("What up?")
-  ->setFrom(array('congratulatinos@gmail.com'))
-  ->setTo(array('congratulatinos@gmail.com'))
-  ->setBody("
-<p>
-  Dude, I'm <b>totally</b> sending you email via AWS.
-</p>
-  ", 'text/html');
- 
-  $mailer->send( $message );
+require_once('vendor/phpmailer/phpmailer/class.phpmailer.php');
+    $to         = "headradio@gmail.com";
+    $from       = "congratulatinos@gmail.com";
+    $subject    = "a test subject";
+    $body       = "Bitch I'm Paid. That's All I Gotta Say. I sent this bitch from AWS.";
+
+    $mail       = new PHPMailer();
+    $mail->IsSMTP(true);            // use SMTP
+
+    //$mail->SMTPDebug  = 2;        // enables SMTP debug information (for testing)
+                                    // 1 = errors and messages
+                                    // 2 = messages only
+    $mail->SMTPAuth   = true;                  // enable SMTP authentication
+    $mail->Host       = "tls://email-smtp.us-east-1.amazonaws.com"; // Amazon SES server, note "tls://" protocol
+    $mail->Port       = 465;                    // set the SMTP port
+    $mail->Username   = "AKIAJ67TTPIBW2QGLLKQ";  // SES SMTP  username
+    $mail->Password   = "ApYOnDy8yTk4FQJ23eRDSAkk8NPBZPN2umsl4sXRMl8I";  // SES SMTP password
+
+    $mail->SetFrom($from, 'First Last');
+    $mail->AddReplyTo($from,'First Last');
+    $mail->Subject    = $subject;
+    $mail->MsgHTML($body);
+    $address = $to;
+    $mail->AddAddress($address, $to);
+
+    if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+      echo "Message sent!";
+    }
+?>
