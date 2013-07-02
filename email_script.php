21dc190d66ea7e4bc886d0355c7f40da7eee8f26
Diego Ardila <ardila@mit.edu>
2013-07-02 06:08:57 +0000

    First public version!

--- email_script.php
+++ email_script.php
@@ -30,25 +30,95 @@ function email_songs($to)
 {
     $from       = "congratulatinos@gmail.com";
     $subject    = "Congratulatinos!";
-    $body       = "<html>
-<head>
-  <title>Test Mail</title>
+    $body       = "<head>
+
+<style>
+
+body {
+
+font-family: \"HelveticaNeueLight\", \"HelveticaNeue-Light\", \"Helvetica Neue Light\", \"HelveticaNeue\", \"Helvetica Neue\", \"TeXGyreHerosRegular\", \"Helvetica\", \"Tahoma\", \"Geneva\", \"Arial\", sans-serif;
+
+font-weight:300;
+
+font-stretch:expanded;
+
+
+
+}
+a {
+  color: #900;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+  position: relative;
+}
+
+</style>
+
+<title>Test Mail</title>
+
 </head>
+
 <body>
-    <p><a href='linktosongs'>Click to download</a><p>
-    <h1> Thanks to: </h1>
-    <a href='http://houseboatt.bandcamp.com/'>Ryan Decker</a>: Drums on Cloudbreak and Protocholic, coined 'Congratulatinos'<br>
-    <a href='https://www.facebook.com/JamesNevilleArt'>James Neville</a>: Album art<br>
-    <a href='saintjulien.bandcamp.com'>Amanda Glasser</a>: 'Sunn' vocals on 'How You Want It'<br>
-    <a href='http://www.drewmazurek.com/Main.html'>Drew Mazurek</a>: Mix Engineer<br>
-    <a href='https://myspace.com/tenelevenths'>Roderick Lauver</a>: Co-wrote bass on Cloudbreak<br>
-    <a href='http://trur.org/background/'>Richard Whalley</a>: Drums on Bittersunset Red, claps on Carmen<br>
-    Wenning Xu: helped with album art<br>
-    <a href='https://soundcloud.com/gilliangrogan'>Gillian Grogan</a>: Got us in the loop<br>
-    Max DeLiso and Josh Zheng: Helped with the website<br>
-    DLu, Tippens: Noise toleration<br>
+
+<p>Shareable links, smallest to largest file size:</p>
+
+<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20V2%5D.zip'>MP3 V2</a> <br>
+<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20V0%5D.zip'>MP3 V0</a> <br>
+<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20320%5D.zip'>MP3 320</a> <br>
+<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BFLAC%5D.zip'>FLAC</a>
+<hr>
+<h3> Thanks to: </h3>
+
+
+
+<a href='http://houseboatt.bandcamp.com' title='Drums on &quot;Cloudbreak&quot; and &quot;Protocholic&quot;, coined &quot;Congratulatinos&quot;'>Ryan Decker</a> <br>
+
+<a href='http://www.youtube.com/user/lawrencepiano100?feature=watch' title='Guest pianist for &quot;Underwater Voyage&quot;'>Lawrence (Liang-Hui) Chu</a><br>
+
+<a href='https://www.facebook.com/JamesNevilleArt' title='Created album art'>James Neville</a> <br>
+
+<a href='http://saintjulien.bandcamp.com' title='&quot;Sunnnnn&quot; vocals on &quot;How You Want It&quot;'>Amanda Glasser</a> <br>
+
+<a href='https://myspace.com/tenelevenths' title='Co-wrote bass on &quot;Cloudbreak&quot;'>Roderick Lauver</a> <br>
+
+<a href='http://trur.org/background' title='Drums on &quot;Bitter Sunset Red&quot;, claps on &quot;Carmen&quot;'>Richard Whalley</a> <br>
+
+<a title='Helped with album art'> Wenning Xu</a>, <a href='http://vimeo.com/sebastianhernandez' title='Helped with album art'>Sebastian Hernandez</a>, <a title='Helped with album art'>Brendan Fay</a>  <br>
+
+<a href='http://static.megafrock.com' title='Helped with the website'>Max DeLiso</a> and <a href='https://github.com/boxcarton'  title='Helped with the website'>Josh Zheng</a><br>
+
+<a title='inadvertent kitchen noises on &quot;Protocholic&quot;, Noise toleration'>DLu</a> <br>
+
+<a href='http://www.drewmazurek.com' title='Mix Engineer'>Drew Mazurek</a> <br>
+
+<hr>
+
+<h3>Further Thanks to: </h3>
+
+<a title='In addition to the main ones (creation, nourishment, education) - for their trust and understanding'>MOM and PAPA (Evelyn and Victor DeLiso)</a> <br>
+
+<a title='Por todo'>Mami and Papi (Patricia and Sergio Ardila)</a> <br>
+
+<a href='https://soundcloud.com/gilliangrogan' title='Got us in the loop'>Gillian Grogan</a><br>
+
+<a href='http://www.acesofmusic.com' title='Instilled a love of harmony'>The Aces</a>, <a href='http://web1.johnshopkins.edu/octopodes' title='Instilled a love of harmony'>JHU Octopodes</a>, <a href='http://itn.mbhs.edu' title='Instilled a love of harmony'>InToneNation</a><br>
+<a title='Noise toleration'>Tippens</a><br>
+
+<a title='Early guitar mentors'>Tim Wenck, Gary Kao, Shivank Gupta, Alex McAbee</a><br>
+
+
+<a>Luke Klingensmith</a><br>
+
+<a href='http://www.leosvirsky.com/' title='Played me &quot;Climbing Up The Walls&quot; in middle school'>Leo Svirsky</a> <br>
+<a href='http://www.dnr.state.md.us/publiclands/western/greenridgeforest.asp' title='Inspiration for and birthplace of &quot;Robber Barons&quot; and &quot;Protocholic&quot;'>Green Ridge State Forest</a> <br>
+
 </body>
-</html>";
+
+</html>
+";
 
     $mail       = new PHPMailer();
     $mail->IsSMTP(true);            // use SMTP
