14b6dcb17e1fb5dcface8c8f77f22a665e928831
Diego Ardila <ardila@mit.edu>
2013-09-22 20:59:26 +0000

    added a function : confirm for only singing up to email list

--- email_script.php
+++ email_script.php
@@ -13,6 +13,10 @@ if ($_REQUEST['functionName'] == 'email_songs_and_subscribe') {
     subscribe($to, $name);
     email_songs($to);
 };
+if ($_REQUEST['functionName'] == 'subscribe'){
+subscribe($to, $name);
+confirm_email($to);
+};
 function subscribe($to, $name){
 echo "I made it in";
 $emailFile = "/var/www/data/emails.txt";
@@ -26,10 +30,10 @@ $formattedFile = "/var/www/data/formatted.txt";
 $formattedfh = fopen($formattedFile, 'ab') or die("can't open file");
 fwrite($formattedfh, $formatted); 
 };
-function email_songs($to)
+function confirm_email($to)
 {
     $from       = "congratulatinos@gmail.com";
-    $subject    = "Congratulatinos!";
+    $subject    = "Mailing List";
     $body       = "<head>
 
 <style>
@@ -62,89 +66,6 @@ a:hover {
 </head>
 
 <body>
-
-<p>Shareable links, smallest to largest file size:</p>
-
-<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20V2%5D.zip'>MP3 V2</a> <br>
-<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20V0%5D.zip'>MP3 V0</a> <br>
-<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BLAME%20MP3%20320%5D.zip'>MP3 320</a> <br>
-<a href='https://dl.dropboxusercontent.com/u/18847953/Congratulatinos%20-%20Congratulatinos%21%20%282013%29%20%5BFLAC%5D.zip'>FLAC</a>
-<hr>
-<h3> Thanks to: </h3>
-
-
-
-<a href='http://houseboatt.bandcamp.com' title='Drums on &quot;Cloudbreak&quot; and &quot;Protocholic&quot;, coined &quot;Congratulatinos&quot;'>Ryan Decker</a> <br>
-
-<a href='http://www.youtube.com/user/lawrencepiano100?feature=watch' title='Guest pianist for &quot;Underwater Voyage&quot;'>Lawrence (Liang-Hui) Chu</a><br>
-
-<a href='https://www.facebook.com/JamesNevilleArt' title='Created album art'>James Neville</a> <br>
-
-<a href='http://saintjulien.bandcamp.com' title='&quot;Sunnnnn&quot; vocals on &quot;How You Want It&quot;'>Amanda Glasser</a> <br>
-
-<a href='https://myspace.com/tenelevenths' title='Co-wrote bass on &quot;Cloudbreak&quot;'>Roderick Lauver</a> <br>
-
-<a href='http://trur.org/background' title='Drums on &quot;Bitter Sunset Red&quot;, claps on &quot;Carmen&quot;'>Richard Whalley</a> <br>
-
-<a title='Helped with album art'> Wenning Xu</a>, <a href='http://vimeo.com/sebastianhernandez' title='Helped with album art'>Sebastian Hernandez</a>, <a title='Helped with album art'>Brendan Fay</a>  <br>
-
-<a href='http://static.megafrock.com' title='Helped with the website'>Max DeLiso</a> and <a href='https://github.com/boxcarton'  title='Helped with the website'>Josh Zheng</a><br>
-
-<a title='inadvertent kitchen noises on &quot;Protocholic&quot;, Noise toleration'>DLu</a> <br>
-
-<a href='http://www.drewmazurek.com' title='Mix Engineer'>Drew Mazurek</a> <br>
-
-<hr>
-
-<h3>Further Thanks to: </h3>
-
-<a title='In addition to the main ones (creation, nourishment, education) - for their trust and understanding'>MOM and PAPA (Evelyn and Victor DeLiso)</a> <br>
-
-<a title='Por todo'>Mami and Papi (Patricia and Sergio Ardila)</a> <br>
-
-<a href='https://soundcloud.com/gilliangrogan' title='Got us in the loop'>Gillian Grogan</a><br>
-
-<a href='http://www.acesofmusic.com' title='Instilled a love of harmony'>The Aces</a>, <a href='http://web1.johnshopkins.edu/octopodes' title='Instilled a love of harmony'>JHU Octopodes</a>, <a href='http://itn.mbhs.edu' title='Instilled a love of harmony'>InToneNation</a><br>
-<a title='Noise toleration'>Tippens</a><br>
-
-<a title='Early guitar mentors'>Tim Wenck, Gary Kao, Shivank Gupta, Alex McAbee</a><br>
-
-
-<a>Luke Klingensmith</a><br>
-
-<a href='http://www.leosvirsky.com/' title='Played me &quot;Climbing Up The Walls&quot; in middle school'>Leo Svirsky</a> <br>
-<a href='http://www.dnr.state.md.us/publiclands/western/greenridgeforest.asp' title='Inspiration for and birthplace of &quot;Robber Barons&quot; and &quot;Protocholic&quot;'>Green Ridge State Forest</a> <br>
-
+The fact that you got this means you are on our email list. REJOICE.
 </body>
 
-</html>
-";
-
-    $mail       = new PHPMailer();
-    $mail->IsSMTP(true);            // use SMTP
-
-    //$mail->SMTPDebug  = 2;        // enables SMTP debug information (for testing)
-                                    // 1 = errors and messages
-                                    // 2 = messages only
-    $mail->SMTPAuth   = true;                  // enable SMTP authentication
-    $mail->Host       = "tls://email-smtp.us-east-1.amazonaws.com"; // Amazon SES server, note "tls://" protocol
-    $mail->Port       = 465;                    // set the SMTP port
-    $mail->Username   = "AKIAJ67TTPIBW2QGLLKQ";  // SES SMTP  username
-    $mail->Password   = "ApYOnDy8yTk4FQJ23eRDSAkk8NPBZPN2umsl4sXRMl8I";  // SES SMTP password
-
-    $mail->SetFrom($from, 'Congratulatinos');
-    $mail->AddReplyTo($from,'Congratulatinos');
-    $mail->Subject    = $subject;
-    $mail->MsgHTML($body);
-    $address = $to;
-    $mail->AddAddress($address, $to);
-    if(!$mail->Send()) {
-      echo "Mailer Error: " . $mail->ErrorInfo;
-    } else {
-      echo "Message sent!";
-    } 
-};
-    
-
-    
-?>
